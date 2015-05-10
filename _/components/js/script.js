1a64fba24ea5e307fb9f0f724ddc726d5c6ca15c
Andrew Hahn <andrevvhahn@gmail.com>
2015-05-10 10:12:26 -0500

    Added navigation dots

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -18,5 +18,15 @@ jQuery(document).ready(function($) {
 
 // fullPage Init & Options ---------------------------------------
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+    	// Navigation
+    	anchors: ['design-process', 'product-definition', 'research-analysis',
+    			 'interaction-design', 'wireframe-prototype', 'usability-evaluation',
+    			  'frontend-programming'],
+    	navigation: true,
+    	navigationPosition: 'right',
+    	navigationTooltips: ['Design Process', 'Product Definition', 'Research Analysis',
+    						 'Interaction Design', 'Wireframe & Prototype', 'Usability Evaluation',
+    						 'Frontend Programming']
+    });
 });
