55c32c5b936774b15b2a8bf8ea03c419f2eb8b67
Andrew Hahn <andrevvhahn@gmail.com>
2015-05-19 12:52:18 -0500

    responsive menu working but still buggy and need to polish it.

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -1,5 +1,36 @@
 // Slider Init & Options ----------------------------------------
 $(document).ready(function($) {
+
+    // fullPage Init & Options ---------------------------------------------------
+    // ---------------------------------------------------------------------------
+    $('#fullpage').fullpage({
+        // Layout
+        verticalCentered: false,
+        // Navigation
+        anchors: ['home', 'product-definition', 'research-analysis', 'interaction-design', 'wireframe-prototype', 'usability-evaluation', 'frontend-programming'],
+        navigation: true,
+        navigationPosition: 'right',
+        slidesNavigation: false,
+        // Scrolling
+        loopHorizontal: false,
+        // Design
+        controlArrows: false
+    });
+
+    // Set austoScrolling to false at tablet and mobile breakpoints
+    $(window).resize(function() {
+
+        if ($(window).width() < 887) {
+            $.fn.fullpage.setAutoScrolling(false);
+        } else {
+            $.fn.fullpage.setAutoScrolling(true);
+        }
+
+    }); //end of resize()
+
+
+    // Slider Init & Options -----------------------------------------------------
+    // ---------------------------------------------------------------------------
 	$('#slider1, #slider2, #slider3, #slider4, #slider5, #slider6').royalSlider({
 		// options
 		imageScaleMode: 'fit',
@@ -11,46 +42,27 @@ $(document).ready(function($) {
 		arrowsNavAutoHide: false,
 		arrowsNavHideOnTouch: true,
 		transitionSpeed: 350,
-		globalCaption: false
+		globalCaption: true
+
 	}); //royalSlider
-});//ready()
 
 
-// fullPage Init & Options ---------------------------------------
-$(document).ready(function() {
-
-    /*
-    function autoScrollOff() {
-        var screenWidth = $(window).width();
-        if (screenWidth < 887) {
-            $.fn.fullpage.setAutoScrolling(false);
-        } else {
-            $.fn.fullpage.setAutoScrolling(true);
-        }
-    }
-    */
-
-	// Options
-    $('#fullpage').fullpage({
-    	// Layout
-    	verticalCentered: false,
-
-    	// Navigation
-    	anchors: ['home', 'product-definition', 'research-analysis', 'interaction-design', 'wireframe-prototype', 'usability-evaluation', 'frontend-programming'],
-    	navigation: true,
-    	navigationPosition: 'right',
-    	slidesNavigation: false,
-
-    	// Scrolling
-    	loopHorizontal: false,
-    	//autoScrolling: false,
-
-    	// Design
-    	controlArrows: false
-
+    // mainNav-mobile ------------------------------------------------------------
+    // ---------------------------------------------------------------------------
+    $(function () {
+        $('.click-nav > ul').toggleClass('no-js js');
+        $('.click-nav .js ul').hide();
+        $('.click-nav .js').click(function(e) {
+            $('.click-nav .js ul').slideToggle(200);
+            $('.clicker').toggleClass('active');
+            e.stopPropagation();
+        });
+        $(document).click(function() {
+            if ($('.click-nav .js ul').is(':visible')) {
+                $('.click-nav .js ul', this).slideUp();
+                $('.clicker').removeClass('active');
+            }
+        });
     });
 
-    //autoScrollOff();
-
-
-});
+});//ready()
