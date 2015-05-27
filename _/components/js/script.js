03f654614ce26b9dbf142698630bda87248746ea
Andrew Hahn <andrevvhahn@gmail.com>
2015-05-27 12:46:02 -0500

    added script to fix layout for short viewports

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -1,8 +1,53 @@
 // Slider Init & Options ----------------------------------------
 $(document).ready(function($) {
 
-    // fullPage Init & Options ---------------------------------------------------
-    // ---------------------------------------------------------------------------
+
+    // Layout fix for short viewport on load -----------------
+    // -------------------------------------------------------
+    if ($(window).height() < 670) {
+        $("#designProcessPageTitle").addClass("shorter-margin");
+        $("#skillsList1").addClass("hide");
+        $("#skillsList2").addClass("hide");
+        $("#skillsList3").addClass("hide");
+        $("#skillsList4").addClass("hide");
+        $("#skillsList5").addClass("hide");
+        $("#skillsList6").addClass("hide");
+    } else {
+        $("#designProcessPageTitle").removeClass("shorter-margin");
+        $("#skillsList1").removeClass("hide");
+        $("#skillsList2").removeClass("hide");
+        $("#skillsList3").removeClass("hide");
+        $("#skillsList4").removeClass("hide");
+        $("#skillsList5").removeClass("hide");
+        $("#skillsList6").removeClass("hide");
+    }
+
+
+    // Layout fix for short viewport on resize ---------------
+    // -------------------------------------------------------
+    $(window).resize(function() {
+
+        if ($(window).height() < 670) {
+            $("#skillsList1").addClass("hide");
+            $("#skillsList2").addClass("hide");
+            $("#skillsList3").addClass("hide");
+            $("#skillsList4").addClass("hide");
+            $("#skillsList5").addClass("hide");
+            $("#skillsList6").addClass("hide");
+            $("#designProcessPageTitle").addClass("shorter-margin");
+        } else {
+            $("#skillsList1").removeClass("hide");
+            $("#skillsList2").removeClass("hide");
+            $("#skillsList3").removeClass("hide");
+            $("#skillsList4").removeClass("hide");
+            $("#skillsList5").removeClass("hide");
+            $("#skillsList6").removeClass("hide");
+            $("#designProcessPageTitle").removeClass("shorter-margin");
+        }
+    });
+
+    // fullPage Init & Options -------------------------------
+    // -------------------------------------------------------
     $('#fullpage').fullpage({
         // Layout
         verticalCentered: false,
@@ -20,7 +65,7 @@ $(document).ready(function($) {
     // Set austoScrolling to false at tablet and mobile breakpoints
     $(window).resize(function() {
 
-        if ($(window).width() < 887 || $(window).height() < 790) {
+        if ($(window).width() < 887) {
             $.fn.fullpage.setAutoScrolling(false);
         } else {
             $.fn.fullpage.setAutoScrolling(true);
@@ -29,8 +74,10 @@ $(document).ready(function($) {
     }); //end of resize()
 
 
-    // Slider Init & Options -----------------------------------------------------
-    // ---------------------------------------------------------------------------
+
+
+    // Slider Init & Options -----------------------------------
+    // ----------------------------------------------------------
 	$('#slider1, #slider2, #slider3, #slider4, #slider5, #slider6').royalSlider({
 		// options
 		imageScaleMode: 'fit',
